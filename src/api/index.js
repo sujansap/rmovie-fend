36fd5e0af0abd1c2be622ba045fb9ee43c4fec67
Sujan Sapkota <sujan.sapkota@student.hogent.be>
2023-11-29 09:35:04 +0100

    baseUrl via env

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,27 @@
 import axios from 'axios';
 
-const baseUrl = `http://localhost:9000/api`; // 👈 1
+//const baseUrl = `http://localhost:9000/api`; 
 
-export async function getAll(url) { // 👈 2
+const baseUrl = import.meta.env.VITE_API_URL;
+
+export async function getAll(url) { 
   const {
     data,
-  } = await axios.get(`${baseUrl}/${url}`); // 👈 3
+  } = await axios.get(`${baseUrl}/${url}`);
 
   
 
   return data.items;
 }
 
-export const save = async (url, { arg: body }) => { // 👈 1
-  await axios.post(`${baseUrl}/${url}`, body); // 👈 2
+export const save = async (url, { arg: body }) => { 
+  await axios.post(`${baseUrl}/${url}`, body); 
+};
+
+export const post = async (url, { arg }) => {
+  const {
+    data,
+  } = await axios.post(`${baseUrl}/${url}`, arg);
+
+  return data;
 };
