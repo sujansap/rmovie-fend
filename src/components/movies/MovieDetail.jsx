086ed25607299d0292580a5830663c1dcf7906c1
Sujan Sapkota <sujan.sapkota@student.hogent.be>
2023-12-13 16:15:41 +0100

    fixed shows n/a for 0 rating

--- src/components/movies/MovieDetail.jsx
+++ src/components/movies/MovieDetail.jsx
@@ -1,11 +1,14 @@
 import { Detail } from "../Detail";
+import { useMemo } from "react";
 
 const MovieDetail = ({ MOVIE, avgRating }) => {
+  let rating = avgRating.rating;
+  rating = rating >= 0 ? rating : -1;
   return (
     <Detail
       title={MOVIE.title}
       poster={MOVIE.poster}
-      rating={avgRating.rating || -1}
+      rating={rating}
       genres={MOVIE.genreMovies || []}
       text={MOVIE.synopsis}
     />
